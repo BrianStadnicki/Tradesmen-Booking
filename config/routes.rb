7caf2f636907e8cd45638c11e0e26a506a99ef6a
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-02 19:33:11 +0100

    remove roles view

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   resources :tradesmen_trades
   resources :tradesmen_profiles
   resources :businesses
-  resources :roles
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
