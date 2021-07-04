152c6d64045c4ec3443111456bad1bfaad8f101e
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-04 21:10:15 +0100

    setup basic abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,15 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     if user.admin?
       can :manage, :all
-    else
-      can :read, :all
+    elsif user.tradesmen?
+      can :index, Business
+      can :read, Business, :all
+      can :read, Job, tradesmen_profile: { id: user.tradesmen_profile.id }
+    elsif user.booker?
+      can :read, Business
     end
   end
 end
