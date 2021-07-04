034a36440b6d8513433a45b75e5146ca4eedfb76
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-04 09:47:06 +0100

    delete invalid associations when tradesmen trade is deleted

--- app/models/tradesmen_trade.rb
+++ app/models/tradesmen_trade.rb
@@ -1,5 +1,5 @@
 class TradesmenTrade < ApplicationRecord
-  has_and_belongs_to_many :tradesmen_profiles
+  has_and_belongs_to_many :tradesmen_profiles, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 20 }
 end
