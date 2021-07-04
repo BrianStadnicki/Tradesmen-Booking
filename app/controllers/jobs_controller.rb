2c27d5fd22dfaf5c75e9968e1698c73ed4c2c111
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-04 15:18:54 +0100

    add job tradesman to view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -46,6 +46,6 @@ class JobsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def job_params
-    params.require(:job).permit(:title, :description, :address, :business_id)
+    params.require(:job).permit(:title, :description, :address, :business_id, :tradesmen_profile_id)
   end
 end
