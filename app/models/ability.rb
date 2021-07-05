53e4909de5ca9d7af88c7239799d379afa92e91e
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-05 22:25:01 +0100

    allow admins to manage tradesmen profile

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,10 @@ class Ability
       can :read, Job, tradesmen_profile: { id: user.tradesmen_profile.id }
       can :read, TradesmenProfile
       can :manage, TradesmenProfile, owner_id: user.id
+      if TradesmenProfileUser.joins(:role).exists? user: user, tradesmen_profile: TradesmenProfile,
+                                                   role: { name: 'Admin' }
+        can :manage, TradesmenProfile
+      end
     elsif user.booker?
       can :read, Business
     end
