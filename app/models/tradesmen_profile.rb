b361853acf922601d911cf5768eed09c2011f700
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-06 19:51:24 +0100

    update tradesmen profile associations to include only users without join entry

--- app/models/tradesmen_profile.rb
+++ app/models/tradesmen_profile.rb
@@ -3,13 +3,20 @@ class TradesmenProfile < ApplicationRecord
   belongs_to :owner, dependent: :destroy, class_name: 'User'
   has_many :jobs
 
-  has_many :users, class_name: 'TradesmenProfileUser'
-  has_many :admins, lambda {
-                      TradesmenProfileUser.joins(:role).where(roles: { name: 'Admin' }).includes(:user)
-                    }, class_name: 'TradesmenProfileUser'
-  has_many :employees, lambda {
-                         TradesmenProfileUser.joins(:role).where(roles: { name: 'Employee' }).includes(:user)
-                       }, class_name: 'TradesmenProfileUser'
+  has_many :users_tradesmen_profile_user, class_name: 'TradesmenProfileUser'
+  has_many :users, through: :users_tradesmen_profile_user
+
+  has_many :admins_tradesmen_profile_user,
+           lambda {
+             TradesmenProfileUser.joins(:role).where(roles: { name: 'Admin' }).includes(:user)
+           }, class_name: 'TradesmenProfileUser'
+  has_many :admins, through: :admins_tradesmen_profile_user, source: :user
+
+  has_many :employees_tradesmen_profile_user,
+           lambda {
+             TradesmenProfileUser.joins(:role).where(roles: { name: 'Employee' }).includes(:user)
+           }, class_name: 'TradesmenProfileUser'
+  has_many :employees, through: :employees_tradesmen_profile_user, source: :user
 
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
   validates :description, presence: true, length: { maximum: 200 }
