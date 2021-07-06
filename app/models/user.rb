86f6eb9aea9de9b5a7047ef0abd5f800f490fff1
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-06 19:50:04 +0100

    add tradesmen profile user and business user assocation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   belongs_to :role
   has_one :business, required: false, foreign_key: 'owner_id', dependent: :destroy
   has_one :tradesmen_profile, required: false, foreign_key: 'owner_id', dependent: :destroy
+  has_one :tradesmen_profile_user, required: false
+  has_one :business_user, required: false
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :address, presence: true, length: { maximum: 100 }
