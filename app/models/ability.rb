6a0dcf3eaba75b03744f3f12d882d776271f7ed2
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-06 06:34:38 +0100

    allow all business' users to manage jobs

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,8 @@ class Ability
       can :manage, :all
     elsif user.tradesmen?
       can :read, Business
-      can :read, Job, tradesmen_profile: { id: user.tradesmen_profile.id }
+      can :manage, Job, tradesmen_profile_id: TradesmenProfileUser.find_by_user_id(user.id)
+
       can :read, TradesmenProfile
       can :manage, TradesmenProfile, owner_id: user.id
       if TradesmenProfileUser.joins(:role).exists? user: user, tradesmen_profile: TradesmenProfile,
