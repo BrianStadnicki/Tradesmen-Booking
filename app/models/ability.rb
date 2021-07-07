d5e90757840e825f089bd019c4f7f61c21850119
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-07 18:08:12 +0100

    add todo to ability about tradesmen profile's users reading other users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,7 +9,7 @@ class Ability
       can :manage, Job, tradesmen_profile: user.tradesmen_profile_user.tradesmen_profile
       can :read, TradesmenProfile
       can :manage, TradesmenProfile, owner: user
-
+      # TODO: allow viewing all users associated to tradesmen profile
       case user.tradesmen_profile_user.role.name
       when 'Owner'
         can :manage, User, user.tradesmen_profile_user.tradesmen_profile.users do
