490236b0bfa9e2b3bda2a8e55d58b0862702445a
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-07 22:09:48 +0100

    setup booker abilities and remove redundant query from owner managing tradesmen profile

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,7 +12,7 @@ class Ability
       # TODO: allow viewing all users associated to tradesmen profile
       case user.tradesmen_profile_user.role.name
       when 'Owner'
-        can :manage, User, user.tradesmen_profile_belongs.users do
+        can :manage, User, user.tradesmen_profile.users do
           true
         end
       when 'Admin'
@@ -23,6 +23,20 @@ class Ability
       end
     elsif user.booker?
       can :read, Business
+      can :manage, Business, owner: user
+      can :manage, Job, business: user.business_belongs
+      can :read, TradesmenProfile
+      case user.business_user.role.name
+      when 'Owner'
+        can :manage, User, user.business.users do
+          true
+        end
+      when 'Admin'
+        can :update, Business, id: user.business_belongs.id
+        can :manage, User, user.business_belongs.employees do
+          true
+        end
+      end
     end
   end
 end
