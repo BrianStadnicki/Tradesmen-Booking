a7f550a10564c362020f26fccc273285c9126b3f
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-08 07:24:31 +0100

    allow viewing all users associated to org and use hash conditions properly rather than blocks

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,34 +8,28 @@ class Ability
       can :read, Business
       can :manage, Job, tradesmen_profile: user.tradesmen_profile_belongs
       can :read, TradesmenProfile
+      can :read, User, tradesmen_profile_user: { tradesmen_profile: user.tradesmen_profile_belongs }
       can :manage, TradesmenProfile, owner: user
-      # TODO: allow viewing all users associated to tradesmen profile
       case user.tradesmen_profile_user.role.name
       when 'Owner'
-        can :manage, User, user.tradesmen_profile.users do
-          true
-        end
+        can :manage, User, tradesmen_profile_user: { tradesmen_profile: user.tradesmen_profile }
       when 'Admin'
         can :update, TradesmenProfile, id: user.tradesmen_profile_belongs.id
-        can :manage, User, user.tradesmen_profile_belongs.employees do
-          true
-        end
+        can :manage, User,
+            tradesmen_profile_user: { tradesmen_profile: user.tradesmen_profile_belongs, role: { name: 'Employee' } }
       end
     elsif user.booker?
       can :read, Business
       can :manage, Business, owner: user
       can :manage, Job, business: user.business_belongs
+      can :read, User, business_user: { business: user.business_belongs }
       can :read, TradesmenProfile
       case user.business_user.role.name
       when 'Owner'
-        can :manage, User, user.business.users do
-          true
-        end
+        can :manage, User, business_user: { business: user.business }
       when 'Admin'
         can :update, Business, id: user.business_belongs.id
-        can :manage, User, user.business_belongs.employees do
-          true
-        end
+        can :manage, User, business_user: { business: user.business, role: { name: 'Employee' } }
       end
     end
   end
