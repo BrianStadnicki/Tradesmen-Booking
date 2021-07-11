667299ba58f8b8408adfe09474602e909cc1d904
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-11 18:05:45 +0100

    remove views for BusinessTradesmenProfile and specify abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,29 +7,35 @@ class Ability
     elsif user.tradesmen?
       can :read, Business
       can :manage, Job, tradesmen_profile: user.tradesmen_profile_belongs
-      can :read, TradesmenProfile
       can :read, User, tradesmen_profile_user: { tradesmen_profile: user.tradesmen_profile_belongs }
+      can :read, BusinessesTradesmenProfile, tradesmen_profile: user.tradesmen_profile_belongs
+      can :read, TradesmenProfile
       can :manage, TradesmenProfile, owner: user
       case user.tradesmen_profile_user.role.name
       when 'Owner'
         can :manage, User, tradesmen_profile_user: { tradesmen_profile: user.tradesmen_profile }
+        can :manage, BusinessesTradesmenProfile, tradesmen_profile: user.tradesmen_profile
       when 'Admin'
         can :update, TradesmenProfile, id: user.tradesmen_profile_belongs.id
         can :manage, User,
             tradesmen_profile_user: { tradesmen_profile: user.tradesmen_profile_belongs, role: { name: 'Employee' } }
+        can :manage, BusinessesTradesmenProfile, tradesmen_profile: user.tradesmen_profile_belongs
       end
     elsif user.booker?
       can :read, Business
       can :manage, Business, owner: user
       can :manage, Job, business: user.business_belongs
       can :read, User, business_user: { business: user.business_belongs }
+      can :read, BusinessesTradesmenProfile, business: user.business_belongs
       can :read, TradesmenProfile
       case user.business_user.role.name
       when 'Owner'
         can :manage, User, business_user: { business: user.business }
+        can :manage, BusinessesTradesmenProfile, business: user.business_belongs
       when 'Admin'
         can :update, Business, id: user.business_belongs.id
         can :manage, User, business_user: { business: user.business, role: { name: 'Employee' } }
+        can :manage, BusinessesTradesmenProfile, business: user.business_belongs
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :businesses_tradesmen_profiles
   devise_for :users
   root to: 'jobs#index'
   resources :users
