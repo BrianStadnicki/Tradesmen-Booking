af98cdd9bffa74630be3e56e2c604deb2fc5caf6
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-15 07:13:47 +0100

    add to businesses as well, only show trust/untrusted if have perms and pre existing relationship

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -8,6 +8,9 @@ class BusinessesController < ApplicationController
 
   # GET /businesses/1
   def show
+    @business_tradesmen_profile =
+      BusinessesTradesmenProfile.find_by(business_id: @business.id,
+                                         tradesmen_profile_id: @current_user.tradesmen_profile_belongs.id)
   end
 
   # GET /businesses/new
