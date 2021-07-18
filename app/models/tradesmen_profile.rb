cb84e1ee073dd305da639e38de6c4f9d3d973b13
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-18 17:06:16 +0100

    adjust tradesmen profile dependencies  - don't delete owner  - don't delete jobs assigned  - delete BusinessTradesmen

--- app/models/tradesmen_profile.rb
+++ app/models/tradesmen_profile.rb
@@ -1,7 +1,7 @@
 class TradesmenProfile < ApplicationRecord
   has_and_belongs_to_many :tradesmen_trades, dependent: :destroy
-  belongs_to :owner, dependent: :destroy, class_name: 'User'
-  has_many :jobs, dependent: :destroy
+  belongs_to :owner, class_name: 'User'
+  has_many :jobs
 
   has_many :users_tradesmen_profile_user, class_name: 'TradesmenProfileUser', dependent: :destroy
   has_many :users, through: :users_tradesmen_profile_user, dependent: :destroy
@@ -18,7 +18,7 @@ class TradesmenProfile < ApplicationRecord
            }, class_name: 'TradesmenProfileUser'
   has_many :employees, through: :employees_tradesmen_profile_user, source: :user
 
-  has_many :businesses_businesses_tradesmen_profiles, class_name: 'BusinessesTradesmenProfile'
+  has_many :businesses_businesses_tradesmen_profiles, class_name: 'BusinessesTradesmenProfile', dependent: :destroy
   has_many :businesses, through: :businesses_businesses_tradesmen_profiles, source: :business
 
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
