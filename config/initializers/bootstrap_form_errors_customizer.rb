6009da6782e18cb89182995ed97a7bf5b14ba2a4
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-19 22:19:58 +0100

    add form error below field

--- config/initializers/bootstrap_form_errors_customizer.rb
+++ config/initializers/bootstrap_form_errors_customizer.rb
@@ -1,10 +1,15 @@
-ActionView::Base.field_error_proc = proc do |html_tag, _instance|
+ActionView::Base.field_error_proc = proc do |html_tag, instance|
   class_attr_index = html_tag.index 'class="'
 
   if class_attr_index
     html_tag.insert class_attr_index + 7, 'is-invalid '
   else
     html_tag.insert html_tag.index('>'), ' class="is-invalid"'
+  end
+  if html_tag.starts_with?('<label')
+    html_tag
+  else
+    html_tag << "<div class=\"invalid-feedback\">#{instance.error_message.first.humanize}</div>".html_safe
   end
 end
 
