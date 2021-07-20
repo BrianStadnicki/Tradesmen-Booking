3b909ca5883cf5f3c27e7d764a6f0235dc298058
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-20 20:37:12 +0100

    clean up ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,25 +6,24 @@ class Ability
       can :manage, :all
     elsif user.tradesmen?
       if user.tradesmen_profile_belongs.present?
+        tradesmen_profile = user.tradesmen_profile || user.tradesmen_profile_belongs
         can :read, Business
-        can :manage, Job, tradesmen_profile: user.tradesmen_profile_belongs
+        can :manage, Job, tradesmen_profile: tradesmen_profile
         cannot :create, Job
-        can :read, User, tradesmen_profile_user: { tradesmen_profile: user.tradesmen_profile_belongs }
-        can :read, BusinessesTradesmenProfile, tradesmen_profile: user.tradesmen_profile_belongs
+        can :read, User, tradesmen_profile_user: { tradesmen_profile: tradesmen_profile }
+        can :read, BusinessesTradesmenProfile, tradesmen_profile: tradesmen_profile
         can :read, TradesmenProfile
         can :manage, TradesmenProfile, owner: user
         cannot :create, TradesmenProfile if user.tradesmen_profile_belongs.present?
         case user.tradesmen_profile_user.role.name
         when 'Owner'
-          can :manage, User, tradesmen_profile_user: { tradesmen_profile: user.tradesmen_profile }
-          can :create, BusinessesTradesmenProfile, tradesmen_profile: user.tradesmen_profile
-          can :destroy, BusinessesTradesmenProfile, tradesmen_profile: user.tradesmen_profile
+          can :manage, User, tradesmen_profile_user: { tradesmen_profile: tradesmen_profile }
+          can :create, :destroy, BusinessesTradesmenProfile, tradesmen_profile: tradesmen_profile
         when 'Admin'
-          can :update, TradesmenProfile, id: user.tradesmen_profile_belongs.id
+          can :update, TradesmenProfile, id: tradesmen_profile.id
           can :manage, User,
-              tradesmen_profile_user: { tradesmen_profile: user.tradesmen_profile_belongs, role: { name: 'Employee' } }
-          can :create, BusinessesTradesmenProfile, tradesmen_profile: user.tradesmen_profile_belongs
-          can :destroy, BusinessesTradesmenProfile, tradesmen_profile: user.tradesmen_profile_belongs
+              tradesmen_profile_user: { tradesmen_profile: tradesmen_profile, role: { name: 'Employee' } }
+          can :create, :destroy, BusinessesTradesmenProfile, tradesmen_profile: tradesmen_profile
         end
         cannot :create, User
       else
@@ -32,23 +31,22 @@ class Ability
       end
     elsif user.booker?
       if user.business_belongs.present?
+        business = user.business || user.business_belongs
         can :read, Business
         can :manage, Business, owner: user
         cannot :create, Business if user.business_belongs.present?
-        can :manage, Job, business: user.business_belongs
-        can :read, User, business_user: { business: user.business_belongs }
-        can :read, BusinessesTradesmenProfile, business: user.business_belongs
+        can :manage, Job, business: business
+        can :read, User, business_user: { business: business }
+        can :read, BusinessesTradesmenProfile, business: business
         can :read, TradesmenProfile
         case user.business_user.role.name
         when 'Owner'
-          can :manage, User, business_user: { business: user.business }
-          can :create, BusinessesTradesmenProfile, business: user.business
-          can :destroy, BusinessesTradesmenProfile, business: user.business
+          can :manage, User, business_user: { business: business }
+          can :create, :destroy, BusinessesTradesmenProfile, business: business
         when 'Admin'
-          can :update, Business, id: user.business_belongs.id
-          can :manage, User, business_user: { business: user.business, role: { name: 'Employee' } }
-          can :create, BusinessesTradesmenProfile, business: user.business_belongs
-          can :destroy, BusinessesTradesmenProfile, business: user.business_belongs
+          can :update, Business, id: business.id
+          can :manage, User, business_user: { business: business, role: { name: 'Employee' } }
+          can :create, :destroy, BusinessesTradesmenProfile, business: business
         end
         cannot :create, User
       else
