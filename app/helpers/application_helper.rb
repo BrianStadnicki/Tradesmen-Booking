bf9cc667a5bf1a6bd60592c7574307c03a15b1a5
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-22 22:06:24 +0100

    use Nokogiri to edit attributes of svg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,20 @@
 module ApplicationHelper
-  def svg(path)
-    File.open("app/assets/images/#{path}", 'rb') do |file|
-      raw file.read
+
+  def svg(filename, options = {})
+    assets = Rails.application.assets
+    asset = assets.find_asset(filename)
+
+    if asset
+      file = asset.source.force_encoding('UTF-8')
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+      svg = doc.at_css 'svg'
+      svg['class'] = options[:class] if options[:class]
+      svg['width'] = options[:width] if options[:width]
+      svg['height'] = options[:height] if options[:height]
+    else
+      doc = "<!-- SVG #{filename} not found -->"
     end
+
+    raw doc
   end
 end
