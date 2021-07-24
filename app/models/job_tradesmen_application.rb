971a05fdc7550b14cda5f11c14b2b764924ae808
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-24 23:10:15 +0100

    job tradesmen application date today or future

--- app/models/job_tradesmen_application.rb
+++ app/models/job_tradesmen_application.rb
@@ -1,4 +1,6 @@
 class JobTradesmenApplication < ApplicationRecord
   belongs_to :job
   belongs_to :tradesmen_profile
+
+  validates :date, presence: true, inclusion: { in: (Date.today..Float::INFINITY) }
 end
