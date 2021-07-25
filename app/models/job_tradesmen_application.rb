62f0ca8057c0c336ed8c9fa59396043b5bca01ea
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-25 17:37:18 +0100

    job app date validation custom message

--- app/models/job_tradesmen_application.rb
+++ app/models/job_tradesmen_application.rb
@@ -2,5 +2,6 @@ class JobTradesmenApplication < ApplicationRecord
   belongs_to :job
   belongs_to :tradesmen_profile
 
-  validates :date, presence: true, inclusion: { in: (Date.today..Float::INFINITY) }
+  validates :date, presence: true
+  validates_inclusion_of :date, in: (Date.today..Float::INFINITY), message: 'Must not be in past'
 end
