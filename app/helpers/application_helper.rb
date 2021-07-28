a04e747bdd40ed126bdfd84b06f4668141d8b3c0
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-28 23:12:14 +0100

    remove useless nokigiri dependency

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,8 @@
 module ApplicationHelper
 
-  def svg(filename, options = {})
-    assets = Rails.application.assets
-    asset = assets.find_asset(filename)
-
-    if asset
-      file = asset.source.force_encoding('UTF-8')
-      doc = Nokogiri::HTML::DocumentFragment.parse file
-      svg = doc.at_css 'svg'
-      svg['class'] = options[:class] if options[:class]
-      svg['width'] = options[:width] if options[:width]
-      svg['height'] = options[:height] if options[:height]
-    else
-      doc = "<!-- SVG #{filename} not found -->"
+  def svg(path)
+    File.open("app/assets/images/#{path}.svg", 'rb') do |file|
+      raw file.read
     end
-
-    raw doc
   end
 end
