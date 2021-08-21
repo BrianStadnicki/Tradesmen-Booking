7c0c132a355c5ad354860f249d36a586c842b6f2
Brian Stadnicki <stadnickibrian@gmail.com>
2021-08-21 17:39:12 +0100

    basic devise_invitable setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable, :invitable
   belongs_to :role
   has_one :business, required: false, foreign_key: 'owner_id', dependent: :destroy
   has_one :tradesmen_profile, required: false, foreign_key: 'owner_id', dependent: :destroy
