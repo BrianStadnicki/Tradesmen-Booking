ad622c01a21300e4f038224f15551ddb30c557f7
Brian Stadnicki <stadnickibrian@gmail.com>
2021-08-26 23:11:28 +0100

    change dummy service worker

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -1,6 +1,19 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
-    .then(function(reg) {
-      console.log('[Companion]', 'Service worker registered!');
+window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js', { scope: "/" }).then(registration => {
+        console.log('ServiceWorker registered: ', registration);
+
+        let serviceWorker;
+        if (registration.installing) {
+            serviceWorker = registration.installing;
+            console.log('Service worker installing.');
+        } else if (registration.waiting) {
+            serviceWorker = registration.waiting;
+            console.log('Service worker installed & waiting.');
+        } else if (registration.active) {
+            serviceWorker = registration.active;
+            console.log('Service worker active.');
+        }
+    }).catch(registrationError => {
+        console.log('Service worker registration failed: ', registrationError);
     });
-}
+});
