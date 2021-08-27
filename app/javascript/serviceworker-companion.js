ef512a7329cebd0eef3cf7ff31eac119ef36b294
Brian Stadnicki <stadnickibrian@gmail.com>
2021-08-27 18:36:19 +0100

    prompt to subscribe notifications and log keys to console

--- app/javascript/serviceworker-companion.js
+++ app/javascript/serviceworker-companion.js
@@ -1,24 +1,42 @@
-window.addEventListener('load', () => {
-    self.Notification.requestPermission().then(function(permission) {
-        if(permission !== 'granted'){
-            throw new Error('Permission not granted for Notification');
-        }
-    });
-    navigator.serviceWorker.register('/service-worker.js', { scope: "/" }).then(registration => {
-        console.log('ServiceWorker registered: ', registration)
+const registerServiceWorker = async () => {
+    const registration = await navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
+    console.log("Service worker registered")
+    return registration
+}
 
-        let serviceWorker
-        if (registration.installing) {
-            serviceWorker = registration.installing
-            console.log('Service worker installing.')
-        } else if (registration.waiting) {
-            serviceWorker = registration.waiting
-            console.log('Service worker installed & waiting.')
-        } else if (registration.active) {
-            serviceWorker = registration.active
-            console.log('Service worker active.')
+const requestNotificationPermission = async (registration) => {
+    const permission = await self.Notification.requestPermission()
+    if(permission !== 'granted') {
+        throw new Error('Permission not granted for Notification');
+    } else {
+        console.log('Permission granted!');
+        try {
+            const options = {
+                applicationServerKey: urlB64ToUint8Array("BD87MR72mMzHzoMk8pekLiZy7gfiKsLFV-fPP5z9XSJxQ7e0vUYLwfoFABuMwoWfVndK4AvpHs7T4KSy2nGjIBA"),
+                userVisibleOnly: true,
+            }
+            const subscriptions = await registration.pushManager.subscribe(options)
+            console.log(JSON.stringify(subscriptions))
+        } catch (err) {
+            console.log('Error while registering notifications', err)
         }
-    }).catch(registrationError => {
-        console.log('Service worker registration failed: ', registrationError)
-    })
-})
+    }
+}
+
+const main = async () => {
+    const registration = await registerServiceWorker()
+    const permission = await requestNotificationPermission(registration)
+}
+
+main()
+
+const urlB64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
+    const rawData = atob(base64)
+    const outputArray = new Uint8Array(rawData.length)
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i)
+    }
+    return outputArray
+}
