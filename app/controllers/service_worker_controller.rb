b807a1acf0c1c8e33a4bd268dd2a5b7b3f296099
Brian Stadnicki <stadnickibrian@gmail.com>
2021-09-03 14:20:24 +0100

    service worker controller skip getting notifications

--- app/controllers/service_worker_controller.rb
+++ app/controllers/service_worker_controller.rb
@@ -1,6 +1,7 @@
 class ServiceWorkerController < ApplicationController
   authorize_resource
   protect_from_forgery except: [:service_worker, :manifest]
+  skip_before_action :get_notifications
 
   def service_worker
   end
