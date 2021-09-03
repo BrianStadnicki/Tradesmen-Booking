deaca0b5f9e69ee63800c7560bb5a4511efa91bc
Brian Stadnicki <stadnickibrian@gmail.com>
2021-09-03 14:11:13 +0100

    notification validation increase max length of type_type and type_category

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,8 +3,8 @@ class Notification < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true, length: { maximum: 100 }
-  validates :type_type, presence: true, length: { maximum: 20 }
-  validates :type_category, presence: true, length: { maximum: 20 }
+  validates :type_type, presence: true, length: { maximum: 40 }
+  validates :type_category, presence: true, length: { maximum: 40 }
   validates :read, inclusion: { in: [true, false] }
   validates :datetime, presence: true
 end
