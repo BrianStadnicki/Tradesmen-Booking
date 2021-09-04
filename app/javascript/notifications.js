85baaa2ec7b11ff722e30860580f74e6877a0b37
Brian Stadnicki <stadnickibrian@gmail.com>
2021-09-04 13:12:55 +0100

    notifications fetch cache current_id gh-3

--- app/javascript/notifications.js
+++ app/javascript/notifications.js
@@ -1,34 +1,40 @@
 document.addEventListener("turbolinks:load", function () {
     let currentPage = 2
     let notificationsLeft = true
-    let requestLock = false
+    let fetchNotificationsRequestLock = false
+    let fetchCurrentIDRequestLock = false
+    let currentID
     document.getElementById("navbarDropdownMenuLinkNotificationsMenu").addEventListener("scroll", function (event) {
-        console.log(notificationsLeft, requestLock)
-        if(notificationsLeft && !requestLock && this.scrollTop + this.clientHeight + 10 >= this.scrollHeight) {
-            console.log("scrolled to bottom")
+        if (notificationsLeft && !fetchNotificationsRequestLock && this.scrollTop + this.clientHeight + 10 >= this.scrollHeight) {
             document.getElementById("navbarDropdownMenuLinkNotificationsMenuLoading").style.display = "block"
-            requestLock = true
-            fetch("/users/current_id")
-                .then(response => response.json())
-                .then(current_id => {
-                    fetch("/users/" + current_id.id + "/notifications?page=" + currentPage)
-                        .then(response => response.text())
-                        .then(doc => new DOMParser().parseFromString(doc, "text/html"))
-                        .then(doc => {
-                            let menuLoading = document.getElementById("navbarDropdownMenuLinkNotificationsMenuLoading")
-                            menuLoading.style.display = "none"
-                            if (doc.body.innerHTML !== "") {
-                                let menu = document.getElementById("navbarDropdownMenuLinkNotificationsMenu")
-                                while (doc.body.childNodes.length) {
-                                    menu.insertBefore(doc.body.firstChild, menuLoading)
-                                }
-                            } else {
-                                notificationsLeft = false
+            if (!currentID && !fetchCurrentIDRequestLock) {
+                fetchCurrentIDRequestLock = true
+                fetch("/users/current_id")
+                    .then(response => response.json())
+                    .then(current_id => {
+                        currentID = current_id.id
+                        fetchCurrentIDRequestLock = false
+                    })
+            } else {
+                fetchNotificationsRequestLock = true
+                fetch("/users/" + currentID + "/notifications?page=" + currentPage)
+                    .then(response => response.text())
+                    .then(doc => new DOMParser().parseFromString(doc, "text/html"))
+                    .then(doc => {
+                        let menuLoading = document.getElementById("navbarDropdownMenuLinkNotificationsMenuLoading")
+                        menuLoading.style.display = "none"
+                        if (doc.body.innerHTML !== "") {
+                            let menu = document.getElementById("navbarDropdownMenuLinkNotificationsMenu")
+                            while (doc.body.childNodes.length) {
+                                menu.insertBefore(doc.body.firstChild, menuLoading)
                             }
-                            currentPage += 1
-                            requestLock = false
-                        })
-                })
+                        } else {
+                            notificationsLeft = false
+                        }
+                        currentPage += 1
+                        fetchNotificationsRequestLock = false
+                    })
+            }
         }
     })
 })
