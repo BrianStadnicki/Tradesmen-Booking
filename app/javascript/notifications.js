2178b2e526e94a584d07994ec19c9e090a5a78d1
Brian Stadnicki <stadnickibrian@gmail.com>
2021-09-04 20:15:39 +0100

    notifications dropdown listener use user id from local storage gh-3

--- app/javascript/notifications.js
+++ app/javascript/notifications.js
@@ -2,39 +2,29 @@ document.addEventListener("turbolinks:load", function () {
     let currentPage = 2
     let notificationsLeft = true
     let fetchNotificationsRequestLock = false
-    let fetchCurrentIDRequestLock = false
-    let currentID
+    let userID = self.localStorage.getItem("user_id")
     document.getElementById("navbarDropdownMenuLinkNotificationsMenu").addEventListener("scroll", function (event) {
         if (notificationsLeft && !fetchNotificationsRequestLock && this.scrollTop + this.clientHeight + 10 >= this.scrollHeight) {
             document.getElementById("navbarDropdownMenuLinkNotificationsMenuLoading").style.display = "block"
-            if (!currentID && !fetchCurrentIDRequestLock) {
-                fetchCurrentIDRequestLock = true
-                fetch("/users/current_id")
-                    .then(response => response.json())
-                    .then(current_id => {
-                        currentID = current_id.id
-                        fetchCurrentIDRequestLock = false
-                    })
-            } else {
-                fetchNotificationsRequestLock = true
-                fetch("/users/" + currentID + "/notifications?page=" + currentPage)
-                    .then(response => response.text())
-                    .then(doc => new DOMParser().parseFromString(doc, "text/html"))
-                    .then(doc => {
-                        let menuLoading = document.getElementById("navbarDropdownMenuLinkNotificationsMenuLoading")
-                        menuLoading.style.display = "none"
-                        if (doc.body.innerHTML !== "") {
-                            let menu = document.getElementById("navbarDropdownMenuLinkNotificationsMenu")
-                            while (doc.body.childNodes.length) {
-                                menu.insertBefore(doc.body.firstChild, menuLoading)
-                            }
-                        } else {
-                            notificationsLeft = false
+
+            fetchNotificationsRequestLock = true
+            fetch("/users/" + userID + "/notifications?page=" + currentPage)
+                .then(response => response.text())
+                .then(doc => new DOMParser().parseFromString(doc, "text/html"))
+                .then(doc => {
+                    let menuLoading = document.getElementById("navbarDropdownMenuLinkNotificationsMenuLoading")
+                    menuLoading.style.display = "none"
+                    if (doc.body.innerHTML !== "") {
+                        let menu = document.getElementById("navbarDropdownMenuLinkNotificationsMenu")
+                        while (doc.body.childNodes.length) {
+                            menu.insertBefore(doc.body.firstChild, menuLoading)
                         }
-                        currentPage += 1
-                        fetchNotificationsRequestLock = false
-                    })
-            }
+                    } else {
+                        notificationsLeft = false
+                    }
+                    currentPage += 1
+                    fetchNotificationsRequestLock = false
+                })
         }
     })
 })
