e0ee4849c25114f68d6718ca5f9c4d7a0f4c2b80
Brian Stadnicki <stadnickibrian@gmail.com>
2021-09-10 06:52:24 +0100

    service worker controller skip unread notification count query

--- app/controllers/service_worker_controller.rb
+++ app/controllers/service_worker_controller.rb
@@ -1,6 +1,7 @@
 class ServiceWorkerController < ApplicationController
   skip_authorization_check
   protect_from_forgery except: [:service_worker]
+  skip_before_action :get_unread_notifications_count
 
   def service_worker
   end
