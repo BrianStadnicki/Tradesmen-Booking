525bea1e301adb48e61f20e6718b14c2605730d8
Brian Stadnicki <stadnickibrian@gmail.com>
2021-09-18 11:42:40 +0100

    increase max len of tradesmen trade's name

--- app/models/tradesmen_trade.rb
+++ app/models/tradesmen_trade.rb
@@ -1,5 +1,5 @@
 class TradesmenTrade < ApplicationRecord
   has_and_belongs_to_many :tradesmen_profiles, dependent: :destroy
 
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 30 }
 end
